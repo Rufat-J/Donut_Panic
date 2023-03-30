046ae06fdec083701612beea85870c7fb407119f
sebastianp85 <109860660+sebastianp85@users.noreply.github.com>
2023-03-30 22:03:05 +0200

    products, restaurant, categories

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
-import express from "express"
-import mongoose from "mongoose"
+import express from 'express';
+import mongoose from 'mongoose';
+import productsRouter from './routes/product-routes.js';
+import restaurantsRoutes from './routes/restaurants-routes.js';
+import categoriesRoutes from './routes/categories-routes.js';
 
-const app = express()
-const conn = "mongodb+srv://donut:12345@donut.vqtqaoi.mongodb.net/test"
+const app = express();
+const conn = 'mongodb+srv://donut:12345@donut.vqtqaoi.mongodb.net/test';
 
-app.get('/', (req, res)=> {
-    res.send('Hello World')
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
-app.listen(3000, ()=>{
-    mongoose.connect(conn, { useNewUrlParser: true, useUnifiedTopology: true }).then((result, error)=> {
-        if(result){
-            console.log('Connected to http://localhost:3000')}
-        else if (error){
-            console.error(error)
-        }
-    })
-} )
+app.use('/restaurants', restaurantsRoutes);
+app.use('/products', productsRouter);
+app.use('/categories', categoriesRoutes);
+
+app.listen(1234, () => {
+  mongoose
+    .connect(conn, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result, error) => {
+      if (result) {
+        console.log('Connected to http://localhost:3000');
+      } else if (error) {
+        console.error(error);
+      }
+    });
+});
