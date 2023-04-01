744163103151243bf3fb476c036e44015dc037d8
Rufat-J <rj.7@live.se>
2023-04-01 21:57:02 +0200

    loginPage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,13 @@ import Home from "./pages/Home.jsx";
 import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from "react-router-dom"
 import Root from './components/Root'
 import ErrorPage from "./components/ErrorPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements([
         <Route path="/" element={<Root/>} errorElement={<ErrorPage/>}>
             <Route index element={<Home/>}/>
+            <Route path="/login" element={<LoginPage/>} />
         </Route>
     ])
 );
