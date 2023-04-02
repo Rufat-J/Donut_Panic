829d7ffb5c073a890afe5791d1c5347fbea71d9e
StromTina <112858034+StromTina@users.noreply.github.com>
2023-04-02 20:42:19 +0200

    rendering donuts and drinks separately

--- client/src/components/MenuCard.jsx
+++ client/src/components/MenuCard.jsx
@@ -4,21 +4,17 @@ import ListGroup from 'react-bootstrap/ListGroup';
 export default function MenuCard({menu}) {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Img src={menu.image} />
             <Card.Body>
                 <Card.Title>{menu.name}</Card.Title>
                 <Card.Text>
                     {menu.ingredients}
                 </Card.Text>
             </Card.Body>
-            <ListGroup className="list-group-flush">
+            <ListGroup>
                 <ListGroup.Item>Price: ${menu.price}</ListGroup.Item>
                 <ListGroup.Item>Category: {menu.category}</ListGroup.Item>
             </ListGroup>
-            <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
         </Card>
     );
 }
--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import MenuCard from "../components/MenuCard.jsx";
+import adminPage from '../styles/adminPage.css'
 
 export default function Menu() {
     const [menu, setMenu] = useState([]);
@@ -14,9 +15,20 @@ export default function Menu() {
         fetchData();
     }, []);
 
+    const donutMenu = menu.filter(menuItem => menuItem.category === "Donuts");
+    const drinkMenu = menu.filter(menuItem => menuItem.category === "drinks");
+
     return (
-        <div>
-            {menu.map(menu => <MenuCard menu={menu}/>)}
+        <div className="admin-page">
+            <h2>Donuts</h2>
+            {donutMenu.map(menuItem => (
+                <MenuCard key={menuItem.id} menu={menuItem} />
+            ))}
+
+            <h2>Drinks</h2>
+            {drinkMenu.map(menuItem => (
+                <MenuCard key={menuItem.id} menu={menuItem} />
+            ))}
         </div>
-    )
+    );
 }
