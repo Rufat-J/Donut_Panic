e0779af65acae67d493958e303c253e35641b450
StromTina <112858034+StromTina@users.noreply.github.com>
2023-04-03 11:30:45 +0200

    added add-to-cart

--- client/src/components/AddToCartButton.jsx
+++ client/src/components/AddToCartButton.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 export default function AddToCartButton({ product }) {
-    const [quantity, setQuantity] = useState(1);
+    const [quantity, setQuantity] = useState(0);
 
     const incrementQuantity = () => {
         setQuantity(quantity + 1);
