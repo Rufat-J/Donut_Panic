82cb2f9d92e86142a9bc5186c577354e4f257892
Rufat-J <rj.7@live.se>
2023-04-11 15:22:25 +0200

    fixed quantity for products on admin orders

--- server/routes/orders-routes.js
+++ server/routes/orders-routes.js
@@ -1,31 +1,24 @@
 import Router from "express";
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
 const ordersRouter = Router();
 
 
 const ordersSchema = new Schema({
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-    },
-    restaurant: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "restaurants",
-    },
-    total_price: Number,
-    status: String,
-    // pickup_time: Date,
+        type: mongoose.Schema.Types.ObjectId, ref: "users",
+    }, restaurant: {
+        type: mongoose.Schema.Types.ObjectId, ref: "restaurants",
+    }, total_price: Number, status: String, // pickup_time: Date,
     // order_time: Date,
-    products: [
-        {
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "products",
-            },
-            quantity: Number,
+    products: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId, ref: "products",
         },
-    ],
+        quantity: Number,
+        name: String,
+        price: Number
+    },],
 });
 
 const OrdersModel = mongoose.model("Orders", ordersSchema);
@@ -39,26 +32,33 @@ ordersRouter.post("/", async (req, res) => {
             restaurant: req.body.restaurant,
             total_price: req.body.totalPrice,
             products: req.body.cartItems,
-            quantity: req.body.quantity
+            quantity: req.body.quantity,
+            price: req.body.price,
             //pickup_time: req.body.pickup_time,
             //order_time: req.body.order_time,
         });
         const createdOrder = await order.save();
         res.status(201).json(createdOrder);
-    }
-    catch (error) {
+    } catch (error) {
         res.status(500).json(error);
     }
 });
 
 ordersRouter.get("/", async (req, res) => {
-    const orders = await OrdersModel.find().populate("user").populate("products").exec();
+    const orders = await
+        OrdersModel.find()
+            .populate("user")
+            .populate("products")
+            .exec();
     res.status(200).json(orders);
 });
 
 ordersRouter.get("/:id", async (req, res) => {
     try {
-        const order = await OrdersModel.findById(req.params.id).populate("user").populate("products").exec();
+        const order = await OrdersModel.findById(req.params.id)
+            .populate("user")
+            .populate("products")
+            .exec();
         res.status(200).json(order);
     } catch (error) {
         res.status(500).json(error);
@@ -66,11 +66,10 @@ ordersRouter.get("/:id", async (req, res) => {
 });
 
 
-
 ordersRouter.delete("/:id", async (req, res) => {
     try {
         await OrdersModel.findByIdAndDelete(req.params.id);
-        res.json({ message: "deleted" });
+        res.json({message: "deleted"});
     } catch (error) {
         res.status(500).json(error);
     }
@@ -78,11 +77,7 @@ ordersRouter.delete("/:id", async (req, res) => {
 
 ordersRouter.patch("/:id", async (req, res) => {
     try {
-        const updatedOrder = await OrdersModel.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true }
-        );
+        const updatedOrder = await OrdersModel.findByIdAndUpdate(req.params.id, req.body, {new: true});
         res.status(200).json(updatedOrder);
     } catch (error) {
         res.status(500).json(error);
