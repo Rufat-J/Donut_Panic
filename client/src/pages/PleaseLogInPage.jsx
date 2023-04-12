390ad9c613874f0d6b2ae025e54c42d9442a9792
StromTina <112858034+StromTina@users.noreply.github.com>
2023-04-12 14:34:55 +0200

    basic styling for please-login-component

--- client/src/pages/PleaseLogInPage.jsx
+++ client/src/pages/PleaseLogInPage.jsx
@@ -1,14 +1,17 @@
 import { useNavigate} from "react-router-dom";
+import '../styles/pleaseLoginPage.css'
 
 function PleaseLogInPage() {
     const Navigate = useNavigate()
 
     return (
-        <div>
+        <div className="please-login">
             <h1>Please Log In or Register</h1>
             <p>You need to log in or register to access the shopping cart.</p>
+            <div className="please-login-buttons">
             <button onClick={() => Navigate('/login')}>Log In</button>
             <button onClick={() => Navigate('/register')}>Register</button>
+            </div>
         </div>
     );
 }
